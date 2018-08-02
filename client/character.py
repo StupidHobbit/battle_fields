fd8fc996d987b40e25b47fb7aed61a36634b3a16
DEDCANNIBAL <godun0202@mail.ru>
2018-08-02 16:25:23 +0500

    Update character module in client: add class Unit. Add small changes to the game module in client

--- client/character.py
+++ client/character.py
@@ -1,27 +1,35 @@
 import pyglet
 import resources
+from utilities import Point
 
-class Character:
-    def __init__(self, class_name: str, nick_name: str, batch, group):
-        self.class_name = class_name
-        self.nick_name = nick_name
+
+class Unit:
+    def __init__(self, name: str, pos: Point=Point(0, 0)):
+        self.name = name
+        self.pos = pos
+        self.v = Point(2, 2)
+
+    def update(self, dt: float):
+        self.pos = self.v * dt
+
+class Character(Unit):
+    def __init__(self, name: str, nick: str, batch, group, pos: Point=Point(0, 0)):
+        Unit.__init__(self, name, pos)
+        self.nick = nick
+        self.batch = batch
+        self.group = group
+        self.max_hp = 2 # Will load
+        self.hp = self.max_hp
+
+    def action(self, name: str, dir: Point):
         pass
 
-    def update(self, time: float):
+    def reaction(self, name: str):
         pass
 
-    def set_data(self, coord):
+    def add_message(self, msg: str):
         pass
 
-    def action(self, action_name: str, dir):
-        pass
-
-    def reaction(self, reaction_name: str):
-        pass
-
-    def add_message(self, message: str):
-        pass
-
-    def set_effects(self, effects: list):
+    def set_effects(self, effect: list):
         pass
 
